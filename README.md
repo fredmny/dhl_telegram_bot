45d44ab28a3c274dd85453b4a790b152377c7e60
Fred <fred.mny@gmail.com>
2021-01-31 13:39:15 -0300

    Developing first functioning Telegram Bot

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Telegram DHL Bot (WIP)
-Another Project with the main intention of learning Python. The purpose of this application is to get updates about my package status through Telegram.
+Another Project with the main intention of learning Python. The purpose of this application is to get updates about my DHL package status through Telegram.
 ## Topics Being Learned
-- Use of API's
-- Writing a Telegram Bot
-- Using Git Branches
+1. Use of API's
+Using the DHL API to get my package status.
+2. Writing a Telegram Bot
+3. Using Git Branches 
+What I am doing here probably is a bit of an overkill, but the idea is to get used to use the 'Develop' and 'feature/[feature]' branches and, in this way, learn to respect the git flow.
--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from DHL to my Telegram account'''
 import os
 import requests
 from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 
 # Load own modules
 import shipment_info
@@ -12,11 +14,54 @@ load_dotenv()
 BOT_KEY = os.getenv('BOT_KEY')
 TRACKN = os.getenv('TRACKING_NUMBER')
 
+# Enable Logging
+logging.basicConfig()
+
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    # Send a message when the command /start is issued.
+    update.message.reply_text('Welcome to the DHL Telegram bot')
+
+def help(update, context):
+    # Send a message when the command /help is issued.
+    update.message.reply_text('Help!')
+
+def error(update, context):
+    # Log errors caused by Updates.
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 # Get the Shimpent Info
-sh_info = shipment_info.get_shipment_info(TRACKN)
+def shipment_status(update, context):
+    sh_info = shipment_info.get_shipment_info(TRACKN)
+    update.message.reply_text(
+    f'--- Shipment Status ---\n\
+    Last update: {sh_info[1]}\n\
+    Status: {sh_info[2]}\n\
+    Message: {sh_info[3]}'
+    )
 
-print(sh_info)
+def main():
+    '''
+    Start the bot
+    '''
+    updater = Updater(BOT_KEY)
 
+    # Get the dispatcher to register handlers (need to understand better)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(CommandHandler('status', shipment_status))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
 
 ''' 
 To Do:
